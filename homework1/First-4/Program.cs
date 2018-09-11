f714cf8e6e430de85f08a04903b18e722275c45c
Jason Lee <1098717970@qq.com>
2018-09-11 19:53:32 +0800

    init

---
+++ homework1/First-4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+           
+            Console.Write("Please write the first number: ");
+            s = Console.ReadLine();
+            double a = Int32.Parse(s);
+            Console.Write("Please write the second number: ");
+            s = Console.ReadLine();
+            double b = Int32.Parse(s);
+            double product = a *b;
+            Console.Write("The product is: "+product);
+        }
+    }
+}
